2dcbbff229c5df3616da0dc6557cadbd9997aca7
Brad Brown <bradbrown@bradbrown.llc>
2024-03-19 21:30:45 -0400

    kvcache vertigo, now using kvvertigo

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ import { AIQ } from 'https://deno.land/x/aiq@0.0.0/mod.ts'
 import { Lazy } from 'https://deno.land/x/lazy_promise@0.0.1/mod.ts'
 import { Gate } from 'https://deno.land/x/gate@0.0.0/mod.ts'
 import { Snail } from 'https://deno.land/x/snail@0.0.0/mod.ts'
+import { KvVertigo } from 'https://deno.land/x/kv_vertigo@0.0.0/mod.ts'
 
 export type Fallbacks<T> = { value:T, expireIn:number }
 
@@ -17,11 +18,11 @@ export class KvCache<T> {
     expireIn?:number
     key:Deno.KvKey
     gate:Gate<void>|null
-    kv:Deno.Kv
+    kv:KvVertigo
     out:AIQ<string>
     err:AIQ<Error>
 
-    constructor(kv:Deno.Kv, key:Deno.KvKey, fallbacks:Fallbacks<T>) {
+    constructor(kv:KvVertigo, key:Deno.KvKey, fallbacks:Fallbacks<T>) {
         this.kv = kv
         this.fallbacks = fallbacks
         this.timestamp = Date.now()
@@ -41,7 +42,7 @@ export class KvCache<T> {
         if (this.gate) return this.value ?? this.fallbacks.value
         this.gate = new Gate()
         
-        const lazy:Lazy<Deno.KvEntryMaybe<KvCacheEntry<T>>> = () => this.kv.get<KvCacheEntry<T>>(this.key)
+        const lazy:Lazy<Error|Deno.KvEntryMaybe<KvCacheEntry<T>>> = () => this.kv.get<KvCacheEntry<T>>(this.key)
         const snail = new Snail(lazy)
         snail.born
             .then(() => this.out.push(`KvCache.get getting key [${this.key}]`))
@@ -49,9 +50,9 @@ export class KvCache<T> {
             .then(value => this.out.push(`KvCache.get got key [${this.key}], value ${JSON.stringify(value,(_,v)=>typeof v=='bigint'?''+v:v)}`))
             .catch(reason => this.err.push(new Error(`KvCache.get failed to retrieve key [${this.key}]`, { cause: reason })))
         const kvem = await snail.lazy().catch(() => {})
-        if (this.gate === null) return this.value ?? this.fallbacks.value
+        // TODO: add output to something's this.err if kvem is error
+        if (this.gate === null || kvem instanceof Error) return this.value ?? this.fallbacks.value
         const cached = kvem?.value
-
         this.value = cached?.value ?? this.value ?? this.fallbacks.value
         this.expireIn = cached?.expireIn ?? this.expireIn ?? this.fallbacks.expireIn
         this.timestamp = Date.now()
@@ -70,7 +71,7 @@ export class KvCache<T> {
         this.expireIn = setter.expireIn
         this.timestamp = Date.now()
 
-        const lazy:Lazy<Deno.KvCommitResult> = () => this.kv.set(this.key, setter)
+        const lazy:Lazy<Error|Deno.KvCommitResult> = () => this.kv.set(this.key, setter)
         const snail = new Snail(lazy)
         snail.born
             .then(() => this.out.push(`KvCache.set setting key [${this.key}] to value ${JSON.stringify(setter,(_,v)=>typeof v=='bigint'?''+v:v)}`))
@@ -78,6 +79,7 @@ export class KvCache<T> {
             .then(() => this.out.push(`KvCache.get set key [${this.key}] to value ${JSON.stringify(setter,(_,v)=>typeof v=='bigint'?''+v:v)}`))
             .catch(reason => this.err.push(new Error(`KvCache.set failed setting key [${this.key}] to value ${JSON.stringify(setter,(_,v)=>typeof v=='bigint'?''+v:v)}`, { cause: reason })))
         snail.lazy().catch(() => {})
+        // TODO: add output to something's this.err if 'snail.lazy().catch(() => {})' is error
 
         this.gate?.resolve()
         this.gate = null
