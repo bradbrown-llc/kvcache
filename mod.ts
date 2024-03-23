ce29112daefff14e90d9f0ca6f4ff9b0a34ecd2f
Brad Brown <bradbrown@bradbrown.llc>
2024-03-22 20:53:58 -0400

    updated imports

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
-import { AIQ } from 'https://deno.land/x/aiq@0.0.0/mod.ts'
-import { Lazy } from 'https://deno.land/x/lazy_promise@0.0.1/mod.ts'
-import { Gate } from 'https://deno.land/x/gate@0.0.0/mod.ts'
-import { Snail } from 'https://deno.land/x/snail@0.0.0/mod.ts'
-import { KvVertigo } from 'https://deno.land/x/kv_vertigo@0.0.0/mod.ts'
+import { AIQ } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/aiq@0.0.0/mod.ts'
+import { Lazy } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/lazy@0.0.0/mod.ts'
+import { Gate } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/gate@0.0.0/mod.ts'
+import { Snail } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/snail@0.0.0/mod.ts'
+import { KvVertigo } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/kvvertigo@0.0.1/mod.ts'
 
 export type Fallbacks<T> = { value:T, expireIn:number }
 
